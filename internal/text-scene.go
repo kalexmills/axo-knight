16ea99e9c79546456e417281670deef1832e06ff
K. Alex Mills <k.alex.mills@gmail.com>
2023-03-27 13:22:37 -0500

    feat: dialogue engine is working

--- internal/text-scene.go
+++ internal/text-scene.go
@@ -2,87 +2,20 @@ package internal
 
 import (
 	"embed"
+	"fmt"
+	"github.com/DrJosh9000/yarn"
+	"github.com/DrJosh9000/yarn/bytecode"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/tinne26/etxt"
 	"golang.org/x/image/math/fixed"
 	"image"
 	"image/color"
+	_ "image/png"
+	"log"
+	"sync"
 	"unicode/utf8"
 )
-
-//
-//// Type alias to create an unexported alias of etxt.Renderer.
-//// This is quite irrelevant for this example, but it's useful in
-//// practical scenarios to avoid leaking a public internal field.
-//type renderer = etxt.Renderer
-//
-//// Wrapper type for etxt.Renderer. Since this type embeds etxt.Renderer
-//// it will preserve all its methods, and we can additionally add our own
-//// new DrawInBox() method.
-//type TextBoxRenderer struct{ renderer }
-//
-//// The new method for TextBoxRenderer. It draws the given text within the
-//// given bounds, performing basic line wrapping on space " " characters.
-//// This is only meant as a reference: this method doesn't split on "-",
-//// very long words will overflow the box when a single word is longer
-//// than the width of the box, \r\n will be considered two line breaks
-//// instead of one, etc. In many practical scenarios you will want to
-//// further customize the behavior of this function. For more complex
-//// examples of Feed usages, see examples/ebiten/typewriter, which also
-//// has a typewriter effect, multiple colors, bold, italics and more.
-//// Otherwise, if you only needed really basic line wrapping, feel free
-//// to copy this function and use it directly. If you don't want a custom
-//// TextBoxRenderer type, it's trivial to adapt the function to receive
-//// a standard *etxt.Renderer as an argument instead.
-////
-//// Notice that this function relies on the renderer's alignment being
-//// (etxt.Top, etxt.Left).
-//func (r *TextBoxRenderer) DrawInBox(text string, bounds image.Rectangle) {
-//	// helper function
-//	var getNextWord = func(str string, index int) string {
-//		start := index
-//		for index < len(str) {
-//			codePoint, size := utf8.DecodeRuneInString(str[index:])
-//			if codePoint <= ' ' {
-//				return str[start:index]
-//			}
-//			index += size
-//		}
-//		return str[start:index]
-//	}
-//
-//	// create Feed and iterate each rune / word
-//	feed := r.renderer.NewFeed(fixed.P(bounds.Min.X, bounds.Min.Y))
-//	index := 0
-//	for index < len(text) {
-//		switch text[index] {
-//		case ' ': // handle spaces with Advance() instead of Draw()
-//			feed.Advance(' ')
-//			index += 1
-//		case '\n', '\r': // \r\n line breaks *not* handled as single line breaks
-//			feed.LineBreak()
-//			index += 1
-//		default:
-//			// get next word and measure it to see if it fits
-//			word := getNextWord(text, index)
-//			width := r.renderer.SelectionRect(word).Width
-//			if (feed.Position.X + width).Ceil() > bounds.Max.X {
-//				feed.LineBreak() // didn't fit, jump to next line before drawing
-//			}
-//
-//			// abort if we are going beyond the vertical working area
-//			if feed.Position.Y.Floor() >= bounds.Max.Y {
-//				return
-//			}
-//
-//			// draw the word and increase index
-//			for _, codePoint := range word {
-//				feed.Draw(codePoint) // you may want to cut this earlier if the word is too long
-//			}
-//			index += len(word)
-//		}
-//	}
-//}
 
 //go:embed gamedata
 var gamedata embed.FS
@@ -96,7 +29,17 @@ const padding = 30
 
 var fontColor = color.White
 
+var (
+	program     *bytecode.Program
+	stringTable *yarn.StringTable
+)
+
+var (
+	fg *ebiten.Image
+)
+
 func init() {
+	fg = loadImg("gamedata/ui-scaled.png")
 	fontLib = etxt.NewFontLibrary()
 	_, _, err := fontLib.ParseEmbedDirFonts("gamedata/fonts", gamedata)
 	if err != nil {
@@ -106,23 +49,81 @@ func init() {
 	renderer.SetFont(fontLib.GetFont("Press Start Regular"))
 	renderer.SetSizePx(fontSize)
 	renderer.SetColor(fontColor)
+
+	program, stringTable, err = yarn.LoadFiles("internal/gamedata/yarn/Main.yarnc", "internal/gamedata/yarn/Main-Lines.csv", "en-US")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadImg(path string) *ebiten.Image {
+	f, err := gamedata.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	im, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ebiten.NewImageFromImage(im)
 }
 
 type TextScene struct {
+	handler *DialogueHandler
+	choices []Choice
+	lastSeq int
+}
+
+type Choice struct {
+	clickBounds image.Rectangle
+	choice      int // integer option in the current dialogue settings.
 }
 
 func NewTextScene() *TextScene {
-	return &TextScene{}
+	return &TextScene{
+		handler: NewDialogueHandler(),
+	}
 }
 
 func (s *TextScene) Update() error {
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButton0) {
+		posx, posy := ebiten.CursorPosition()
+		for _, choice := range s.choices {
+			if image.Pt(posx, posy).In(choice.clickBounds) {
+				fmt.Println("sending choice:", choice.choice)
+				s.handler.choice <- choice.choice // send choice back to dialogue handler
+			}
+		}
+	}
 	return nil
 }
 
+var DialogueBounds = image.Rect(146+5, 430+12, 146+517, 430+163)
+
 func (s *TextScene) Draw(screen *ebiten.Image) {
-	s.LayoutNode(screen, Node{
-		Prompt: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-	})
+
+	screen.DrawImage(fg, nil) // draw foreground over everything.
+
+	renderer.SetTarget(screen)
+	s.handler.mut.RLock()
+	func() {
+		feed := renderer.NewFeed(fixed.P(DialogueBounds.Min.X, DialogueBounds.Min.Y))
+		defer s.handler.mut.RUnlock()
+		DrawInBox(feed, s.handler.currNode.Prompt, DialogueBounds)
+		feed.LineBreak()
+		s.choices = make([]Choice, len(s.handler.currOpts))
+		for i, choice := range s.handler.currOpts {
+			str, err := stringTable.Render(choice.Line)
+			if err != nil {
+				panic(fmt.Errorf("rendering option: %w", err))
+			}
+			feed.LineBreak()
+			s.choices[i].clickBounds = DrawInBox(feed, fmt.Sprintf("> %s", str), DialogueBounds)
+			s.choices[i].choice = i
+
+			s.choices[i].clickBounds.Max.X = DialogueBounds.Max.X
+		}
+	}()
 }
 
 type Node struct {
@@ -130,12 +131,107 @@ type Node struct {
 	Choices []string
 }
 
-func (s *TextScene) LayoutNode(screen *ebiten.Image, n Node) {
-	renderer.SetTarget(screen)
-	DrawInBox(n.Prompt, image.Rect(500, 12+padding, 800-padding, 600))
+func (s *TextScene) Layout(w, h int) (int, int) {
+	return 800, 600
 }
 
-func DrawInBox(text string, bounds image.Rectangle) {
+type DialogueHandler struct {
+	vm       *yarn.VirtualMachine
+	mut      sync.RWMutex
+	seq      int // ever-increasing sequence number
+	currNode Node
+	choice   chan int
+	currOpts []yarn.Option
+}
+
+func NewDialogueHandler() *DialogueHandler {
+	result := &DialogueHandler{
+		choice: make(chan int),
+	}
+	result.vm = &yarn.VirtualMachine{
+		Program: program,
+		Handler: result,
+		Vars:    make(yarn.MapVariableStorage),
+	}
+	go func() {
+		fmt.Println("running dialogue handler")
+		if err := result.vm.Run("Start"); err != nil {
+			panic(fmt.Errorf("error from Yarn VM: %w", err))
+		}
+	}()
+	return result
+}
+
+func (h *DialogueHandler) NodeStart(nodeName string) error {
+	fmt.Println("dialogue handler: start", nodeName)
+	return nil
+}
+
+func (h *DialogueHandler) PrepareForLines(lineIDs []string) error {
+	//fmt.Println("dialogue handler: prepare for lines", lineIDs)
+	return nil
+}
+
+func (h *DialogueHandler) Line(line yarn.Line) error {
+	str, err := stringTable.Render(line)
+	if err != nil {
+		return err
+	}
+	h.mut.Lock()
+	func() {
+		defer h.mut.Unlock()
+		h.currNode = Node{
+			Prompt: str.String(),
+		}
+	}()
+	return nil
+}
+
+func (h *DialogueHandler) Update() error {
+	fmt.Println("dialogue handler: update")
+	return nil
+}
+
+func (h *DialogueHandler) Options(opts []yarn.Option) (int, error) {
+	var choices []string
+	for _, opt := range opts {
+		str, err := stringTable.Render(opt.Line)
+		if err != nil {
+			return 0, err
+		}
+		choices = append(choices, str.String())
+	}
+	h.mut.Lock()
+	func() {
+		defer h.mut.Unlock()
+		h.currNode.Choices = choices
+		h.currOpts = opts
+		h.seq++
+	}()
+	fmt.Println("dialogue handler: blocking on selection")
+	choice := <-h.choice
+	fmt.Printf("dialogue handler: selected choice [%d]\n", choice)
+	return choice, nil
+}
+
+func (h *DialogueHandler) NodeComplete(name string) error {
+	fmt.Println("node complete")
+	return nil
+}
+
+func (h *DialogueHandler) DialogueComplete() error {
+	fmt.Println("dialogue complete")
+	return nil
+}
+
+func (h *DialogueHandler) Command(cmd string) error {
+	fmt.Printf("running command: %s\n", cmd)
+	return nil
+}
+
+// DrawInBox draws the provided string staying within the provided bounds. The rectangle taken up by the text written is
+// returned.
+func DrawInBox(feed *etxt.Feed, text string, bounds image.Rectangle) image.Rectangle {
 	// helper function
 	var getNextWord = func(str string, index int) string {
 		start := index
@@ -148,9 +244,15 @@ func DrawInBox(text string, bounds image.Rectangle) {
 		}
 		return str[start:index]
 	}
+	used := image.Rectangle{}
+	used.Min.X = feed.Position.X.Ceil()
+	used.Min.Y = feed.Position.Y.Floor()
+	used.Max = used.Min
 
 	// create Feed and iterate each rune / word
-	feed := renderer.NewFeed(fixed.P(bounds.Min.X, bounds.Min.Y))
+	if feed == nil {
+		feed = renderer.NewFeed(fixed.P(bounds.Min.X, bounds.Min.Y))
+	}
 	index := 0
 	for index < len(text) {
 		switch text[index] {
@@ -170,8 +272,10 @@ func DrawInBox(text string, bounds image.Rectangle) {
 
 			// abort if we are going beyond the vertical working area
 			if feed.Position.Y.Floor() >= bounds.Max.Y {
-				return
+				return used
 			}
+			used.Max.X = max(used.Max.X, (feed.Position.X + width).Ceil())
+			used.Max.Y = max(used.Max.Y, feed.Position.Y.Floor()+12)
 
 			// draw the word and increase index
 			for _, codePoint := range word {
@@ -180,8 +284,5 @@ func DrawInBox(text string, bounds image.Rectangle) {
 			index += len(word)
 		}
 	}
-}
-
-func (s *TextScene) Layout(w, h int) (int, int) {
-	return 800, 600
+	return used
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+ysc compile -o internal/gamedata/yarn yarn/Main.yarn
+
 go run cmd/game/main.go
